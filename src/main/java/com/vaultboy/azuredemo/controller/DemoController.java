4cba7fcd69580910de0d3bd64d2ca6a3d42af54c
Bilal <bilal.nizamani@royalcyber.com>
2024-10-08 19:40:51 +0500

    adding controller

---
+++ src/main/java/com/vaultboy/azuredemo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.vaultboy.azuredemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/demo")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<?> getDemoObject(){
+        return ResponseEntity.ok(Map.of("name", "bilal"));
+    }
+}
